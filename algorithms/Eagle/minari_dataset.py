68d5183eccbd9f98896273d7f979ab4c31641628
Buttontheduck <batinmuslucode@gmail.com>
2026-04-22 03:06:05 +0200

    minari dataset prepare is added

---
+++ algorithms/Eagle/minari_dataset.py
@@ -0,0 +1,45 @@
+import minari
+import gymnasium as gym
+import numpy as np
+
+
+id = "mujoco/halfcheetah/simple-v0"
+
+
+def prepare_minari_data(env_id):
+        
+    minari_dataset = minari.load_dataset(env_id)
+
+    env = minari_dataset.recover_environment()
+
+
+
+    observations = []
+    next_observations = []
+    actions = []
+    rewards = []
+    truncations = []
+    terminations = []
+
+    for eps in minari_dataset.iterate_episodes():
+
+        
+        observations.append(eps.observations[:-1])
+        actions.append(eps.actions)
+        next_observations.append(eps.observations[1:])
+        rewards.append(eps.rewards)
+        truncations.append(eps.truncations)
+        terminations.append(eps.terminations)
+
+    dataset = {
+            "observations": np.concatenate(observations),
+            "actions": np.concatenate(actions),
+            "rewards": np.concatenate(rewards),
+            "next_observations": np.concatenate(next_observations),
+            "truncations": np.concatenate(truncations),
+            "terminations": np.concatenate(terminations),
+
+    }    
+    return env, dataset
+
+
